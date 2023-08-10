8e9e78052651e406b9e06551391a51f8837006a4
saraswathi234 <saraswathisaraswathi81330@gmail.com>
2023-08-10 09:59:36 +0530

    path update

--- src/main/java/day11/solved/ConnectionUtil.java
+++ src/main/java/day11/solved/ConnectionUtil.java
@@ -4,18 +4,18 @@ package day11.solved;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConnectionUtil {
 	 
     public static Connection getConnection()  {
  
         Connection con = null;
-        String url = "jdbc:mysql://localhost/YOURDB";
-        String userName = "USERNAME";
-        String passWord = "PASSWORD";
+        String url = "jdbc:mysql://aws.connect.psdb.cloud:3306/?user=0owaiy5ijln63mkne70g";
+        String userName = "0owaiy5ijln63mkne70g";
+        String passWord = "pscale_pw_T8gfEDfs3s1l8pLgOoA4QpBWI8kRa5bibPnQ0WcgJOs";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(url, userName, passWord);
